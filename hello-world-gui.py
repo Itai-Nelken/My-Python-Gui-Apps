befc5898f6aa42d046c81816f844a7f283e1bca8
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-13 23:18:03 +0200

    Add files via upload

---
+++ hello-world-gui.py
@@ -0,0 +1,6 @@
+from guizero import App, Text
+
+app = App(title="hello, world!")
+message = Text(app, text="welcome to my python gui coded using guizero")
+
+app.display()
---
+++ wanted-poster/wanted-gui.py
@@ -0,0 +1,12 @@
+from guizero import App, Text, Picture
+
+app = App("Wanted!")
+app.bg = "#aaaaaa"
+wanted_text = Text(app, "WANTED")
+wanted_text.text_size = 50
+wanted_text.font="Times New Roman"
+burglar = Picture(app, image="burglar.png")
+
+
+
+app.display()
