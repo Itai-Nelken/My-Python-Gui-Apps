cbc50796c3d231bed71f14440a97dbeedcd83975
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2020-11-13 23:21:11 +0200

    Add files via upload

--- wanted-poster/wanted-gui.py
+++ wanted-poster/wanted-gui.py
@@ -2,6 +2,7 @@ from guizero import App, Text, Picture
 
 app = App("Wanted!")
 app.bg = "#aaaaaa"
+app.height = 610
 wanted_text = Text(app, "WANTED")
 wanted_text.text_size = 50
 wanted_text.font="Times New Roman"
